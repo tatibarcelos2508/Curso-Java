ea5d69980006edee770301a6fb2194a6e46d0a99
tatiane <tatianebarcelos@hotmail.co.uk>
2024-05-07 17:44:08 -0300

    atualizando os projetos

---
+++ Consumo/src/main/java/NumerosReais.java
@@ -0,0 +1,9 @@
+public class NumerosReais {
+    public static void main(String[] args) {
+        int[] vetor= {1,2,3,4,5,6,7,8,9,10};
+
+        for(int i = vetor.length-1; i>=0; i--){
+            System.out.println(vetor[i]);
+        }
+    }
+}
--- Consumo/src/main/java/numerosInteiros.java
+++ Consumo/src/main/java/numerosInteiros.java
@@ -1,2 +1,10 @@
 public class numerosInteiros {
+    public static void main(String[] args) {
+        int[] vetor= {1,2,3,4,5};
+
+        for(int i=0; i < vetor.length; i++){
+
+            System.out.println(vetor[i]);
+        }
+    }
 }
